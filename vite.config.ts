13b1eb7c3c20ef6f518e11d42a73fe729b3a04d6
Abid Ali KP <abidalikp98@gmail.com>
2025-03-04 20:28:05 +0530

    gh pagess

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    host: "0.0.0.0", // Listen on all network interfaces
-    port: 5173, // Optional: Change the port if needed
-    strictPort: true, // Optional: Fail if the port is already in use
-  },
+  base: "/drop-board/",
+  // server: {
+  //   host: "0.0.0.0", // Listen on all network interfaces
+  //   port: 5173, // Optional: Change the port if needed
+  //   strictPort: true, // Optional: Fail if the port is already in use
+  // },
 });
